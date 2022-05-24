9f3e038dc2fd162f1a44d0f2812adfe236f95ce1
Suraj Mandal <surajmandalcell@gmail.com>
2022-05-24 21:59:48 +0530

    update 405 to 400

--- pages/api/quiz/index.ts
+++ pages/api/quiz/index.ts
@@ -3,28 +3,26 @@ import { addQuiz, getAllQuiz, getSingleQuiz } from '@/src/services/db';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    try {
-        switch (req.method) {
-            case 'GET':
-                let data;
-                const { quizId } = req.query;
-                if (quizId) data = await getSingleQuiz(quizId);
-                else data = await getAllQuiz();
-                return res.status(200).json(data);
-            case 'POST':
-                const user = await adminAuth.verifyIdToken(
-                    req.headers.token as string
-                );
-                const quizData = { ...req.body, userId: user.uid };
-                await addQuiz(quizData);
-                return res.status(200).json('Quiz added successfully...');
-            default:
-                throw Error();
-        }
-    } catch (error) {
-        res.status(405).json({ status: false, message: 'Invalid Request' });
+  try {
+    switch (req.method) {
+      case 'GET':
+        let data;
+        const { quizId } = req.query;
+        if (quizId) data = await getSingleQuiz(quizId);
+        else data = await getAllQuiz();
+        return res.status(200).json(data);
+      case 'POST':
+        const user = await adminAuth.verifyIdToken(req.headers.token as string);
+        const quizData = { ...req.body, userId: user.uid };
+        await addQuiz(quizData);
+        return res.status(200).json('Quiz added successfully...');
+      default:
+        throw Error();
     }
+  } catch (error) {
+    res.status(400).json({ status: false, message: 'Invalid Request' });
+  }
 }
