6b37099331f75a6a56cff0d4400c101a9f6b6676
Suraj Mandal <surajmandalcell@gmail.com>
2022-06-04 00:38:36 +0530

    update website

--- src/routes/ai/customgen.ts
+++ src/routes/ai/customgen.ts
@@ -1,10 +1,12 @@
-import { BASE_URL } from '@/src/constants/base';
 import axios from 'axios';
 
 export async function customGen({ article }: { article: string }) {
   console.log({ article });
-  const res = await axios.post(`${BASE_URL}/api/ai/generatecustomqa`, {
-    article,
-  });
+  const res = await axios.post(
+    'http://localhost:3000/api/ai/generatecustomqa',
+    {
+      article,
+    }
+  );
   return res.data;
 }
