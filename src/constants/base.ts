1da8642a2c81dc94b6e6e87a79468f0967df252f
Suraj Mandal <surajmandalcell@gmail.com>
2022-06-04 02:27:51 +0530

    dumb mistake

--- src/constants/base.ts
+++ src/constants/base.ts
@@ -3,4 +3,4 @@ export const BASE_URL =
     ? 'http://0.0.0.0:3000'
     : 'https://brisk.surajmandal.in';
 
-export const BASE_URL2 = 'http://0.0.0.0/3000';
+export const BASE_URL2 = 'http://0.0.0.0:3000';
